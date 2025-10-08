f9106e660ae5467395e90c116f7da9610a7981f7
IOPress <iprogrammer@iopress.info>
2025-10-08 08:51:33 +0100

    add yield

--- C12/client2/client2.c
+++ C12/client2/client2.c
@@ -89,7 +89,7 @@ void socketReadTimeout(int sockfd, char buffer[], int len, int timeoutsec)
             printf("read error %X\n", errno);
             break;
         }
-
+        vTaskDelay(2);
     } while (true);
 }
 
