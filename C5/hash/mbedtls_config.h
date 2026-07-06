c73430b0be1719bddfd7b5f48ffc6c4d763dc69e
IOPress <iprogrammer@iopress.info>
2026-07-06 16:22:21 +0100

    Update mbedtls_config.h for SDK compatibility
    Added configuration options for mbedtls 3.x compatibility.

--- C5/hash/mbedtls_config.h
+++ C5/hash/mbedtls_config.h
@@ -8,4 +8,8 @@
 //hash 
 #define MBEDTLS_SHA256_C
 
+//Needed for sdk 2.2 and 2.3 mbedtls 3.x
+#define MBEDTLS_ALLOW_PRIVATE_ACCESS
+#define MBEDTLS_PLATFORM_MS_TIME_ALT
+
 #include "/home/mike/.pico-sdk/sdk/2.1.1/lib/mbedtls/include/mbedtls/check_config.h"
